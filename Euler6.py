086662f853eaf1cc236d3f5a5d8d02ecaa5877e6
M1k3 Yang <yang.ye.cn@gmail.com>
2013-04-24 14:21:37 +0800

    add a shorter version of solution 6

--- Euler6.py
+++ Euler6.py
@@ -1,1 +1,2 @@
-print(abs(reduce(lambda x, y: x+y, map(lambda x: x**2, range(1, 101))) - reduce(lambda x, y: x+y, range(1, 101))**2))
+#print(abs(reduce(lambda x, y: x+y, map(lambda x: x**2, range(1, 101))) - reduce(lambda x, y: x+y, range(1, 101))**2))
+print(abs(sum([x**2 for x in range(1, 101)]) - sum(range(1, 101))**2))
